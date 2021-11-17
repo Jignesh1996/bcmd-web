25dbb97a95aa19549fbcc3fc8dce83c77c82e12f
zio <jignesh.mistry.759976@gmail.com>
2021-11-16 23:46:15 -0500

    Update run.py
    change the import path of flask _endpoint viewpoint function

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 from app import app
 import pprint
+import flask.scaffold
+import flask
+flask.helpers._endpoint_from_view_func = flask.scaffold._endpoint_from_view_func
+import flask_restful
 
 if __name__ == '__main__':
     try:
